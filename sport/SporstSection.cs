3e6e8b64802c04d1d3b83f419fb2342cb9616d93
rustam-11-2 <rustamkumykov05@gmail.com>
2026-06-09 11:19:27 +0300

    Создан класс SporstSection, в отдельном файле

---
+++ sport/SporstSection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sport
+{
+    public class SporstSection
+    {
+        public string CoachName { get; set; }
+        public string SectionName { get; set; }
+        public int MaxCountParticipants { get; set; }
+
+        public SporstSection(string coach, string sectionName, int maxCountParticipants) 
+        {
+            CoachName = coach;
+            SectionName = sectionName;
+            MaxCountParticipants = maxCountParticipants;
+        }
+
+    }
+}
